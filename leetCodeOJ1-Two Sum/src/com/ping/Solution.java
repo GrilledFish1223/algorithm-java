7b612b44ca1d40977771b0acf47aee4b3c8a4560
Shengping-Zhang <17682319366@163.com>
2018-11-30 18:05:36 +0800

    add   longestCommonPrefix  method

--- leetCodeOJ1-Two Sum/src/com/ping/Solution.java
+++ leetCodeOJ1-Two Sum/src/com/ping/Solution.java
@@ -22,7 +22,7 @@ public class Solution {
 //        for (int i = 0; i < result.length; i++) {
 //            System.out.print(result[i] + "\t");
 //        }
-        System.out.println(romanToInt("III"));
+//        System.out.println(digestFor(3));
 
     }
 
@@ -149,4 +149,27 @@ public class Solution {
 
         return result;
     }
+
+    /**
+     *Write a function to find the longest common prefix string amongst an array of strings.
+     *
+     * If there is no common prefix, return an empty string "".
+     *
+     * Example 1:
+     * Input: ["flower","flow","flight"]
+     * Output: "fl"
+     *
+     * Example 2:
+     * Input: ["dog","racecar","car"]
+     * Output: ""
+     * Explanation: There is no common prefix among the input strings.
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+
+
+
 }
